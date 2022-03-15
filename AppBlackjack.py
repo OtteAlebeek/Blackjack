877288030a9519cdc7de57a0a33f5554e928648d
OtteAlebeek <79521577+OtteAlebeek@users.noreply.github.com>
2022-03-15 09:25:09 +0100

    Add files via upload

---
+++ AppBlackjack.py
@@ -0,0 +1,131 @@
+from select import select
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pickle
+
+Predictor1 = pickle.load(open('Downloads/NaiveBayesPrediction1.pkl', 'rb'))
+Predictor2 = pickle.load(open('Downloads/NaiveBayesPrediction2.pkl', 'rb'))
+Predictor3 = pickle.load(open('Downloads/NaiveBayesPrediction3.pkl', 'rb'))
+
+st.markdown("""
+# Blackjack Predictor
+
+This is the main page for the Blackjack Predictor. On this page you can select how many cards you have in hand,
+so you can make a choice for the right predictor.
+
+There are 3 different predictors where you can choose for:
+    
+First prediction
+    
+In this prediction you get to know if you need to hit or stand in a game of blackjack after receiving your first 2 cards.
+
+Second prediction
+    
+In this prediction you get to know if you need to hit or stand in a game of blackjack after receiving your first 3 cards.
+
+Third prediction
+    
+In this prediction you get to know if you need to hit or stand in a game of blackjack after receiving your first 4 cards.
+
+
+""")
+
+selected_page = st.selectbox("Select the prediction", ("Home", "First", "Second", "Third"))
+
+if selected_page == 'First':
+
+    st.write("""
+    # First prediction
+    On this page you will get to know if you need to hit or stand after receiving your first 2 cards.
+
+    Fill in the filters and see what the best strategy for your next step is
+    """)
+ 
+    def user_input_features():
+     card1 = st.text_input("The first card you get", 0)
+     card2 = st.text_input("The second card you get", 0)
+     dealcard1 = st.text_input("The first card the dealer get", 0)
+     ply2cardsum = st.text_input("Sum of the first 2 cards you get", 0)
+     data = {'The first card you get': card1,
+            'The second card you get': card2,
+            'The first card the dealer get': dealcard1,
+            'Sum of the first 2 cards you get': ply2cardsum}
+     features = pd.DataFrame(data, index=['Values'])
+     return features
+
+    df = user_input_features()
+    st.write(df)
+
+    if st.button('Submit'):
+        Predictor1_load = Predictor1.predict(df)
+        data = {'Blackjack load': Predictor1_load}
+        features = pd.DataFrame(data, index=['Values $'])
+        st.write(features)
+
+elif selected_page == 'Second':
+
+    st.write("""
+    # Second prediction
+    On this page you will get to know if you need to hit or stand after receiving your first 3 cards.
+
+
+    Fill in the filters and see what the best strategy for your next step is
+    """)
+ 
+    def user_input_features():
+     card1 = st.text_input("The first card you get", 0)
+     card2 = st.text_input("The second card you get", 0)
+     card3 = st.text_input("The third card you get", 0)
+     dealcard1 = st.text_input("The first card the dealer get", 0)
+     ply3cardsum = st.text_input("Sum of the first 2 cards you get", 0)
+     data = {'The first card you get': card1,
+            'The second card you get': card2,
+            'The third card you get': card3,
+            'The first card the dealer get': dealcard1,
+            'Sum of the first 3 cards you get': ply3cardsum}
+     features = pd.DataFrame(data, index=['Values'])
+     return features
+
+    df = user_input_features()
+    st.write(df)
+
+    if st.button('Submit'):
+        Predictor2_load = Predictor2.predict(df)
+        data = {'Blackjack load': Predictor2_load}
+        features = pd.DataFrame(data, index=['Values $'])
+        st.write(features)
+elif selected_page == 'Third':
+
+    st.write("""
+    # Third prediction
+    On this page you will get to know if you need to hit or stand after receiving your first 4 cards.
+
+
+    Fill in the filters and see what the best strategy for your next step is
+    """)
+
+    def user_input_features():
+        card1 = st.text_input("The first card you get", 0)
+        card2 = st.text_input("The second card you get", 0)
+        card3 = st.text_input("The third card you get", 0)
+        card4 = st.text_input("The fourth card you get", 0)
+        dealcard1 = st.text_input("The first card the dealer get", 0)
+        ply4cardsum = st.text_input("Sum of the first 2 cards you get", 0)
+        data = {'The first card you get': card1,
+            'The second card you get': card2,
+            'The third card you get': card3,
+            'The fourth card you get': card4,
+            'The first card the dealer get': dealcard1,
+            'Sum of the first 4 cards you get': ply4cardsum}
+        features = pd.DataFrame(data, index=['Values'])
+        return features
+
+    df = user_input_features()
+    st.write(df)
+
+    if st.button('Submit'):
+        Predictor3_load = Predictor3.predict(df)
+        data = {'Blackjack load': Predictor3_load}
+        features = pd.DataFrame(data, index=['Values $'])
+        st.write(features)
